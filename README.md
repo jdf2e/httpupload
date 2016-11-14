3ef91c7b34023e0b3615679cb546efb1a3652921
catalsdevelop <catalsdevelop@jd.com>
2016-11-14 15:02:06 +0800

    增加package.json和server.js文件 增加README.md

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # httpupload
-server side  for jdf output command usde in http mode
+
+jdf http方式上传代码的服务器接收端
+
+# 部署和使用
+
+先clone代码，然后到目录下`npm i`安装依赖，用pm2或者nodemon启动。
+pm2下先关闭当前的实例
+```
+$ pm2 delete instance-name
+$ kill -9 pm2-pid
+```
+修改pm2的启动用户方式
+```
+$ vi  /etc/init.d/pm2-init.sh
+```
+修改 USER=xxxx 和 export PM2_HOME="/var/www/html/.pm2"为你自己的配置，切换到有权限的用户
+```
+$ su xxxx 
+```
+启动 pm2，如果需要修改路径和监听端口的话，到server.js里面去修改，可以配合nginx做反向代理
+```
+$ pm2 startup centos
+$ pm2 start server.js
+```
+
+一定要注意启动选择的用户，要想和ftp sftp方式无缝切换的话，要配置上传目录的权限，不要使用nologin用户，最好是所有用户都在一个分组中，然后上传路径直接chmod 777。
+
+客户端上传时可以直接
+```
+$ jdf output        // 默认上传方式为 http
+$ jdf output -t ftp // 指定上传方式为ftp
+$ jdf output -t scp // 指定上传方式为scp
+```
---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const http = require('http');
+const Busboy = require('busboy');
+const path = require('path');
+const fs = require('fs');
+const fsPath = require('fs-path');
+const port = 8000;
+
+const server = http.createServer((req, res) => {
+    if(req.method == 'POST' && req.url == '/') {
+        const busbody = Busboy({headers: req.headers});
+        busbody.on('file', (fieldname, file, encoding, mimetype) => {
+            let buffers = [];
+            file.on('data',(data) => {
+                buffers.push(data);
+            });
+            file.on('end', () => {
+                const buffer = Buffer.concat(buffers);
+                fsPath.writeFile(fieldname, buffer);
+            });
+        });
+        busbody.on('finish', () => {
+            res.writeHead(200);
+            res.end();
+        })
+        req.pipe(busbody);
+    }
+    else {
+        res.writeHead(400);
+        res.end();
+    }
+});
+
+server.listen(port);
+console.log(`start http sync server on http://localhost:${port}`)
